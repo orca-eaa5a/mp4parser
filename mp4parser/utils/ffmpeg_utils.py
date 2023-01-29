d60b3f14545cc476d2b9415602b49650e9b5cf9e
orca-eaa5a <dlfguswn@naver.com>
2023-01-29 18:47:11 +0900

    fix path-name bug

--- mp4parser/utils/ffmpeg_utils.py
+++ mp4parser/utils/ffmpeg_utils.py
@@ -22,9 +22,13 @@ def ffmpeg_sync(media_file, trim_result, synced=True):
     else:
         ffmpeg_sync_cmd = "{ffmpeg_bin} -y -i {media_file} -itsoffset {ts} -i {media_file} -map 0:v -map 1:a -c copy {out_file}"
 
-    f, ext = os.path.splitext(media_file)
+    _path = os.path.dirname(media_file)
+    f_name = os.path.basename(media_file)
+    f, ext = os.path.splitext(f_name)
+    
     tmp_file_name = hashlib.sha256(f.encode()).hexdigest()
-    out_file = "{}{}".format(tmp_file_name, ext)
+    out_file = "{}/{}{}".format(_path, tmp_file_name, ext)
+
     cmd = ffmpeg_sync_cmd.format(media_file=media_file, ts=sync_delay, out_file=out_file, ffmpeg_bin=FFMPEG_PATH)
     child = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (stdout, stderr) = child.communicate()
@@ -34,9 +38,13 @@ def ffmpeg_sync(media_file, trim_result, synced=True):
     return out_file, sp
 
 def ffmpeg_cutoff_extra_times(media_file, sp, duration):
-    f, ext = os.path.splitext(media_file)
+    _path = os.path.dirname(media_file)
+    f_name = os.path.basename(media_file)
+    f, ext = os.path.splitext(f_name)
+    
     tmp_file_name = hashlib.sha256(f.encode()).hexdigest()
-    out_file = "{}{}".format(tmp_file_name, ext)
+    out_file = "{}/{}{}".format(_path, tmp_file_name, ext)
+    
     ffmpeg_cutoff_cmd = "{ffmpeg_bin} -y -ss {sp} -i {media_file} -c copy -t {duration} {out_file}"
     cmd = ffmpeg_cutoff_cmd.format(media_file=media_file, sp=sp, duration=duration, out_file=out_file, ffmpeg_bin=FFMPEG_PATH)
     
